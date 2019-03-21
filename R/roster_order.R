5fb6fe9a755b43da1041cf145b343428e3086674
shojiwan <shojinaga.ryan@deq.state.or.us>
2019-03-21 16:53:48 -0700

    Update roster_order.R

--- R/roster_order.R
+++ R/roster_order.R
@@ -1,7 +1,12 @@
 roster <- c('Ryan S',
             'Chamille H',
             'Smita M',
-            'Anna W'
-            )
+            'Anna W',
+            'Janani G',
+            'Brian F',
+            'Josh E',
+            'Dave F',
+            'Kat M')
 
 order <- sample(roster)
+order
