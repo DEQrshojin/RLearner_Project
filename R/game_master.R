396f12f53294cf883d46d9647c38c2709e81dab6
Ryan Shojinaga <shojinaga.ryan@deq.state.or.us>
2020-08-04 16:20:33 -0700

    test update

--- R/game_master.R
+++ R/game_master.R
@@ -1,5 +1,5 @@
 game <- function(start = T) {
 
-  if(!start) {stop('You are no fun.')}
+  if(!start) {stop('You are no fun.')} else {print('Awesome! Let\'s get started.')}
 
 }
---
+++ R/game_scratch.R
@@ -0,0 +1,5 @@
+source("C:/Users/rshojin/Desktop/RMS/003_deq/github/RLearner_Project/R/game_master.R")
+
+game(F)
+
+game(T)
