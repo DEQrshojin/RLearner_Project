e8ed0f947cad6e9b215e3d86f6ec5ef6e56aa769
Ryan Shojinaga <shojinaga.ryan@deq.state.or.us>
2020-08-04 17:23:24 -0700

    Preliminary game prompts

--- R/game_master.R
+++ R/game_master.R
@@ -1,5 +1,27 @@
 game <- function(start = T) {
 
-  if(!start) {stop('You are no fun.')} else {print('Awesome! Let\'s get started.')}
+  if(!start) {cat('Well, you\'re no fun.'); stop_quietly()}
+
+  cat('Awesome! Let\'s get started.')
+
+  name <- readline(prompt = "Okay, then, what is your name? ")
+
+  cat(paste0('Okay, ', name, '. This game consist of answering three simple\n',
+             'logic questions or riddles to get an entirely underwhelming pri',
+             'ze.\n'))
+
+  moveOn <- readline(prompt = 'Are you ready (y/n)? ')
+
+  if(moveOn %in% c('n', 'N', 'no', 'No')) {cat('Bummer'); stop_quietly()}
+
+}
+
+stop_quietly <- function() {
+
+  opt <- options(show.error.messages = FALSE)
+
+  on.exit(options(opt))
+
+  stop()
 
 }
